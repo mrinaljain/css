694ec14824cf1522a2e3e2b1499039e07cede99e
mrinaljain <mrinaljain@live.in>
2018-10-15 10:42:26 +0530

    javascript tutorials

---
+++ assets/js/datatypes.js
@@ -0,0 +1,14 @@
+"use strict";
+var sym = Symbol('Hello');
+//console.log(typeof sym);
+
+var undi;
+
+var number = 3;
+
+var string = "lol"
+
+console.log('Undefined variable = ' + undi);
+console.log('Undefined + Number = ' + number + undi);
+console.log('String + Undefined = ' + string + undi);
+console.log('String + Number = ' + string + number);
